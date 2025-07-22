1263211ab3a3a10da3815e9b7dc1ef8af6b7d030
FrotaLucas <lucasdiasfrota@gmail.com>
2025-07-22 16:58:43 +0200

    add code

--- LeetCodeSolutions/Problems/TdaRational.cs
+++ LeetCodeSolutions/Problems/TdaRational.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace LeetCodeSolutions.Problems
 {
 
+    //BEE 1022
     public class TdaRational
     {
         public string Inputs = "4; 1 / 2 + 3 / 4; 2 / 3 * 4 / 5; 6 / 8 - 1 / 4; 5 / 6 / 7 / 8";
