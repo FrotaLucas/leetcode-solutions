290a8c6aa668ab4017617f94456d8bc9775f57b7
FrotaLucas <lucasdiasfrota@gmail.com>
2025-07-22 15:27:34 +0200

    parse variables

--- LeetCodeSolutions/Problems/TdaRational.cs
+++ LeetCodeSolutions/Problems/TdaRational.cs
@@ -9,12 +9,35 @@ namespace LeetCodeSolutions.Problems
 
     public class TdaRational
     {
-        public string Inputs = "4 1 / 2 + 3 / 4  2 / 3 * 4 / 5  6 / 8 - 1 / 4  5 / 6 / 7 / 8";
+        public string Inputs = "4; 1 / 2 + 3 / 4; 2 / 3 * 4 / 5; 6 / 8 - 1 / 4;  5 / 6 / 7 / 8";
 
 
-        public TdaRational()
+        public TdaRational() //depois tentar passar parametro pelo CTOR
         {
             
+        }
+
+
+        public void printTdaRational()
+        {
+            string[] parts = Inputs.Split(";");
+
+            int n = int.Parse(parts[0]);
+
+
+            foreach (string part in parts) {
+
+
+                //part[7] 7. elemento sempre eh a operacao
+                switch (part[7]) { 
+                 
+                }
+            
+            
+            }
+
+
+          
         }
 
     }
