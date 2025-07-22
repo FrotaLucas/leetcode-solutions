4ca4bdca6a6828684c051686fcb6f22eed966cbb
FrotaLucas <lucasdiasfrota@gmail.com>
2025-07-22 14:51:32 +0200

    create class and cosntructor

---
+++ LeetCodeSolutions/Problems/TdaRational.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Problems
+{
+
+    public class TdaRational
+    {
+        public string Inputs = "4 1 / 2 + 3 / 4  2 / 3 * 4 / 5  6 / 8 - 1 / 4  5 / 6 / 7 / 8";
+
+
+        public TdaRational()
+        {
+            
+        }
+
+    }
+}
--- LeetCodeSolutions/obj/Debug/net8.0/LeetCodeSolutions.AssemblyInfo.cs
+++ LeetCodeSolutions/obj/Debug/net8.0/LeetCodeSolutions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCodeSolutions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e2ba7222cc1b26fec517fd4996486de6ef8801")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCodeSolutions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCodeSolutions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
