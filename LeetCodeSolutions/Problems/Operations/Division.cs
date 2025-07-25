00b708065797b89ce1cb3dbbbbbe5250579525c7
FrotaLucas <lucasdiasfrota@gmail.com>
2025-07-25 18:21:56 +0200

    fix DIvision

--- LeetCodeSolutions/Problems/Operations/Division.cs
+++ LeetCodeSolutions/Problems/Operations/Division.cs
@@ -17,7 +17,7 @@ namespace LeetCodeSolutions.Problems.Operations
         public override void DoOperation()
         {
             int numerator = int.Parse(Numerator1) * int.Parse(Denominator2);
-            int denominator = int.Parse(Denominator2) * int.Parse(Numerator2);
+            int denominator = int.Parse(Denominator1) * int.Parse(Numerator2);
 
 
             Console.WriteLine($"Division: {numerator}/{denominator}");
