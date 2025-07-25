c65746223db2370de0c14f1c76627742a501156b
FrotaLucas <lucasdiasfrota@gmail.com>
2025-07-25 17:35:10 +0200

    finish class multiplication

---
+++ LeetCodeSolutions/Problems/Operations/Multiplication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Problems.Operations
+{
+    public class Multiplication : Operation
+    {
+
+        public Multiplication() {
+            TypeOperation = "*";
+        }
+
+
+        public override void DoOperation()
+        {
+            int numerator = int.Parse(Numerator1) * int.Parse(Numerator2);
+            var denominator = int.Parse(Denominator1) * int.Parse(Denominator2);
+
+            Console.WriteLine($"Multiplication: {numerator}/{denominator}");
+        }
+    }
+}
