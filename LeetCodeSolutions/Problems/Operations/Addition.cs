736c0fa5db8437b07e6ea965099a68fc345450e0
FrotaLucas <lucasdiasfrota@gmail.com>
2025-07-25 17:16:46 +0200

    fix Addition class

--- LeetCodeSolutions/Problems/Operations/Addition.cs
+++ LeetCodeSolutions/Problems/Operations/Addition.cs
@@ -10,9 +10,31 @@
         public override void DoOperation()
         {
 
-            int sum = int.Parse(Numerator1) + int.Parse(Numerator2);
+            var n1 = int.Parse(Numerator1);
+            var n2 = int.Parse(Numerator2);
 
-            Console.WriteLine("sum:" + sum);
+            var d1 = int.Parse(Denominator1);
+            var d2 = int.Parse(Denominator2);
+
+            //encontrar MDC
+            int a = d1; int b = d2;
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            int mdc = a;
+
+            int mmc = d1 * d2 / mdc;
+
+            int numerator = n1 * (mmc / d1) + n2 * (mmc / d2);
+            //result = $"{numerator}/{mmc}";
+
+
+            //int sum = int.Parse(Numerator1) + int.Parse(Numerator2);
+
+            Console.WriteLine($"sum: {numerator}/{mmc} ");
         }
     }
 }
