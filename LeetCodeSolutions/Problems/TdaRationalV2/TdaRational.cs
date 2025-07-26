61377259bd07534628080b86aa538e1fc6ef1a3d
FrotaLucas <lucasdiasfrota@gmail.com>
2025-07-26 13:59:21 +0200

    implement methoda addition

--- LeetCodeSolutions/Problems/TdaRationalV2/TdaRational.cs
+++ LeetCodeSolutions/Problems/TdaRationalV2/TdaRational.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Channels;
 using System.Threading.Tasks;
+using LeetCodeSolutions.Problems.TdaRationalV2.Operations;
 
 namespace LeetCodeSolutions.Problems.TdaRationalV2
 {
@@ -17,6 +18,15 @@ namespace LeetCodeSolutions.Problems.TdaRationalV2
 
         public void PrintResult()
         {
+
+            var calculation = new Calculation();
+
+            //substituicao de LISKOV
+            calculation.Operation = new Addition();
+
+            var result1 = calculation.Operation;
+            result1.DoOperation();
+
             Console.WriteLine("teste");
         }
     }
