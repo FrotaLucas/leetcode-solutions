5618126e858059c0322c67343054b1eb796619b6
FrotaLucas <lucasdiasfrota@gmail.com>
2025-07-26 13:59:56 +0200

    implement property Operation

--- LeetCodeSolutions/Problems/TdaRationalV2/Calculation.cs
+++ LeetCodeSolutions/Problems/TdaRationalV2/Calculation.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LeetCodeSolutions.Problems.TdaRationalV2.Operations;
 
 namespace LeetCodeSolutions.Problems.TdaRationalV2
 {
     public class Calculation
     {
-        
+        public Operation Operation { get; set; }    
     }
 }
