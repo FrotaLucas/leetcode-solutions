31bc2b1e01eabf286799ff12d48d6563fbc29dce
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-13 17:04:21 +0100

    new task

---
+++ LeetCodeSolutions/Problems/CoderByte/LongestWordTask.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCodeSolutions.Problems.CoderByte
+{
+    public class LongestWordTask
+    {
+
+        public string LongestWord(string sen)
+        {
+            string[] array = sen.Split(' ');
+            string longestWord = string.Empty;
+
+            foreach (string word in array)
+            {
+                var cleanWord = Regex.Replace(word, "[^a-zA-Z]", "");
+
+                if (cleanWord.Length > longestWord.Length)
+                    longestWord = cleanWord;
+            }
+
+            // code goes here  
+            return longestWord;
+        }
+    }
+}
