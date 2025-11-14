4e6d8394742b962e90bae259787f41ef6d36dff9
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-14 14:00:03 +0100

    finish convert function

--- LeetCodeSolutions/Problems/Codibility/FindBinaryGap.cs
+++ LeetCodeSolutions/Problems/Codibility/FindBinaryGap.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace LeetCodeSolutions.Problems.Codibility
 {
-    internal class FindBinaryGap
+    public class FindBinaryGap
     {
+        public void Solve(int number)
+        {
+            var arrayOfZeros = ConvertIntegerToBinary(number).Split("1");
+
+
+
+        }
+
+
+        public string ConvertIntegerToBinary(int number) 
+        {
+            string result = string.Empty;
+
+            while(number > 0)
+            {
+                var binaryDigit = number % 2;
+                number = number / 2;
+
+                result = binaryDigit + result;
+            }
+        
+
+            return result;
+        }
+
     }
 }
--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -2,6 +2,7 @@
 using LeetCodeSolutions.Problems.BeeCrowd.TdaRationalV1;
 using LeetCodeSolutions.Problems.BeeCrowd.TdaRationalV2;
 using LeetCodeSolutions.Problems.CoderByte;
+using LeetCodeSolutions.Problems.Codibility;
 
 
 
@@ -12,3 +13,6 @@ tda1.Solve();
 var tda2 =  new TdaRationalTaskV2();
 tda2.Solve();
 
+var binary = new FindBinaryGap();
+binary.Solve(9);
+binary.Solve(1041);
