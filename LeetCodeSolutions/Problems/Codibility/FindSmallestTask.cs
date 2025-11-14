b77c0a45adfe13f60618ec81e96d4b11853d28c0
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-14 18:36:17 +0100

    create new class

---
+++ LeetCodeSolutions/Problems/Codibility/FindSmallestTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Problems.Codibility
+{
+    public class FindSmallestTask
+    {
+        //T3 find the smallest
+        // This is a demo task.
+
+        // Write a function:
+
+        // class Solution { public int solution(int[] A); }
+
+        // that, given an array A of N integers, returns the smallest missing positive integer (greater than 0) that does not occur in A.
+
+        // For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+        // Given A = [1, 2, 3], the function should return 4.
+
+        // Given A = [−1, −3], the function should return 1.
+
+        // Write an efficient algorithm for the following assumptions:
+
+        // N is an integer within the range [1..100,000];
+        // each element of array A is an integer within the range [−1,000,000..1,000,000].
+
+        public int Solve(int[] A)
+        {
+
+            return 0;
+        }
+    }
+}
