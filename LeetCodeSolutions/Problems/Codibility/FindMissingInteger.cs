ada5d403f029ce4d33e411ac12061039b409b1f7
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-15 22:56:09 +0100

    new task

---
+++ LeetCodeSolutions/Problems/Codibility/FindMissingInteger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Problems.Codibility
+{
+    // list of integers no nullable
+    public class FindMissingInteger
+    {
+        public int Solve(int[]A)
+        {
+            var n  = A.Length;
+            var refSummUp = n * (n + 1) / 2;
+
+            var realSummup = A.Sum();
+
+            var missingValue = refSummUp - realSummup;
+
+            return missingValue;
+
+        }
+    }
+}
