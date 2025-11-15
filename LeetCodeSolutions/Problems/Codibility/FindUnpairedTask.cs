3572005c8caba45b907cae57696bf6621a26745c
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-15 23:12:10 +0100

    new task

--- LeetCodeSolutions/Problems/Codibility/FindUnpairedTask.cs
+++ LeetCodeSolutions/Problems/Codibility/FindUnpairedTask.cs
@@ -8,5 +8,17 @@ namespace LeetCodeSolutions.Problems.Codibility
 {
     public class FindUnpairedTask
     {
+        //list A with non nullable
+        public int Solve(int[]A)
+        {
+            int n = (A.Length -1)/ 2;
+            var upperSummup = 2 * (1 + n) * (1 + 1 + n) / 2;
+
+
+            var realSummup = A.Sum();
+
+            return upperSummup - realSummup;
+
+        }
     }
 }
