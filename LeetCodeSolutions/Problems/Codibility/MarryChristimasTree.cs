7fe170139480fbf686f1f02d10b42764e3177732
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-16 19:24:33 +0100

    add new task

---
+++ LeetCodeSolutions/Problems/Codibility/MarryChristimasTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Problems.Codibility
+{
+    //print a christimas tree with given height 
+    public class MarryChristimasTree
+    {
+        public void Solve(int height)
+        {
+            string width = "*********";
+            string tree = "";
+
+            for (int i = 0; i < height; i++)
+            {
+                var layer = width + "\n";
+                tree = tree + layer;
+            }
+
+            Console.WriteLine(width);
+        }
+    }
+}
