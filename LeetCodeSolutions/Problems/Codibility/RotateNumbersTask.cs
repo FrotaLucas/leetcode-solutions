fafa26fefe982079d8ac29acfa1ae37bf627f996
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-16 17:08:27 +0100

    add new task

---
+++ LeetCodeSolutions/Problems/Codibility/RotateNumbersTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Problems.Codibility
+{
+    // The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
+
+    // Write a function:
+    // function solution(A: number[], K: number): number[];
+    // that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
+
+    // For example, given
+
+    //     A = [3, 8, 9, 7, 6]
+    //     K = 3
+
+    // the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+
+    public class RotateNumbersTask
+    {
+        public int[] Solve(int[]A, int k)
+        {
+            int[] rotate = A;
+
+            return rotate;
+        }
+    }
+}
