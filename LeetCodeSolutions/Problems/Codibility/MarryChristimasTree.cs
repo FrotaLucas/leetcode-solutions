54dbd729d92ecfe90deb0d28d917be35946926ff
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-17 01:04:06 +0100

    implement method

--- LeetCodeSolutions/Problems/Codibility/MarryChristimasTree.cs
+++ LeetCodeSolutions/Problems/Codibility/MarryChristimasTree.cs
@@ -11,16 +11,41 @@ namespace LeetCodeSolutions.Problems.Codibility
     {
         public void Solve(int height)
         {
-            string width = "*********";
-            string tree = "";
+            //string baseWidth = string.Empty;
+
+            string tree = string.Empty;
+
+            //for(int b = 0; b < 2*height; b++)
+            //{
+            //    baseWidth = baseWidth + "*";
+            //}
+
+            
+            int baseWidth = 2*height;
+            int dots = 0;
 
             for (int i = 0; i < height; i++)
             {
+                var width = string.Empty;
+
+                for(int j = baseWidth -1; j >=0; j--)
+                {
+                    width = width + " ";
+                }
+
+                dots = 2*height - baseWidth;
+                for(int p=0; p <= dots; p++)
+                {
+                    width = width + "*";
+                }
+
+                baseWidth--;
+
                 var layer = width + "\n";
                 tree = tree + layer;
             }
 
-            Console.WriteLine(width);
+            Console.WriteLine(tree);
         }
     }
 }
