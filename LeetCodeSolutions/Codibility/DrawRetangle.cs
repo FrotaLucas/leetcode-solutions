b5811ce0f8abbdd2c466b94cb6a08415bc03aa1d
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-19 12:31:05 +0100

    set method

--- LeetCodeSolutions/Codibility/DrawRetangle.cs
+++ LeetCodeSolutions/Codibility/DrawRetangle.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace LeetCodeSolutions.Codibility
 {
-    internal class DrawRetangle
+    // Exercise:
+    // A string S receives commands to move forward, move backward,
+    // turn right, and turn left. Develop a function that receives
+    // a set of commands and determines whether these commands
+    // will form a rectangle or not (true/false).
+    // Rules: there are no diagonal movements.
+    // I am assuming that the rectangle always starts at point (0,0)
+
+    public class DrawRetangle
     {
+        public bool Solve(string commands)
+        {
+
+
+            return true;
+        }
     }
 }
