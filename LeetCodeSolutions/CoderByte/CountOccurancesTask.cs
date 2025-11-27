e137cdd4c0963029e584dac6ee644aa686d7bebc
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-27 14:09:45 +0100

    new task

---
+++ LeetCodeSolutions/CoderByte/CountOccurancesTask.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeSolutions.CoderByte
+{
+    //    Task
+
+    //Write a function that, given a string (X) containing an array of N semi-colon(;) separated integers, returns a semi-colon separated string of:
+
+    //the number of integers occurring multiple times
+
+    //the lowest duplicate integer(0 if no duplicates)
+
+    //the highest duplicate integer(0 if no duplicates)
+
+    //elements of the input integers without duplicates.The ORDERING of the items must not change.If numbers appear multiple times, the first occurring integer has to be preserved.
+
+    //You solution must have O(n) runtimes. Write the code in C#
+
+
+    //example
+    //"1;3;2;3;4;1;5;3"
+    //return
+
+    //2;1;3;1;3;2;4;5
+    //Explanation:
+
+    //Numbers duplicated: 1, 3 → count = 2
+
+    //Lowest duplicate = 1
+
+    //Highest duplicate = 3
+
+    //Input without duplicates: 1;3;2;4;5
+
+    public class CountOccurancesTask
+    {
+        public string Solve(string str)
+        {
+            return str;
+        }
+    }
+}
