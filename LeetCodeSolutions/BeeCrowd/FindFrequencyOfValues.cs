22f4382cdfed6cc1303627d40b8390abc7f28fab
FrotaLucas <lucasdiasfrota@gmail.com>
2025-12-11 23:57:59 +0100

    create method

---
+++ LeetCodeSolutions/BeeCrowd/FindFrequencyOfValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.BeeCrowd
+{
+    public class FindFrequencyOfValues
+    {
+        public Dictionary<string, int> Solve(string str)
+        {
+            var dictionary = new Dictionary<string, int>();
+
+
+
+            return dictionary;
+        }
+    }
+}
