1811bc19b6ad35d998bf933ef6833a2c638da240
FrotaLucas <lucasdiasfrota@gmail.com>
2025-12-12 00:58:17 +0100

    create second solution

--- LeetCodeSolutions/BeeCrowd/FindFrequencyOfValues.cs
+++ LeetCodeSolutions/BeeCrowd/FindFrequencyOfValues.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LeetCodeSolutions.BeeCrowd
@@ -10,9 +11,23 @@ namespace LeetCodeSolutions.BeeCrowd
     {
         public Dictionary<string, int> Solve(string str)
         {
+            //similar to HashMap
             var dictionary = new Dictionary<string, int>();
 
+            var list = str.Select( elem => elem.ToString()).ToList();
 
+            Console.WriteLine(list);
+
+
+            //foreach (var item in list)
+            //{
+            //    dictionary[item]++;
+            //}
+
+
+            //sol 2
+            var dictionary2 = new Dictionary<string, int>();
+            dictionary2 = str.Select(elem => elem.ToString()).ToList().GroupBy(elem => elem).ToDictionary(group => group.Key, group => group.Count());
 
             return dictionary;
         }
