b50872d779799492664073b9da3552a18a01e13a
FrotaLucas <lucasdiasfrota@gmail.com>
2025-12-13 11:21:01 +0100

    create class

---
+++ LeetCodeSolutions/BeeCrowd/FindLongestCommonSubtringTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.BeeCrowd
+{
+    public class FindLongestCommonSubtringTask
+    {
+        public int Solve(string str1, string str2)
+        {
+            int tot = 0;
+
+
+
+            return 0;
+        }
+
+    }
+}
