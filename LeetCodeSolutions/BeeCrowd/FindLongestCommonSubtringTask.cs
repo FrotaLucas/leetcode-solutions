1d9bd6f471b87eeb86908346161d91b107826470
FrotaLucas <lucasdiasfrota@gmail.com>
2025-12-13 11:23:57 +0100

    add comment

--- LeetCodeSolutions/BeeCrowd/FindLongestCommonSubtringTask.cs
+++ LeetCodeSolutions/BeeCrowd/FindLongestCommonSubtringTask.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.VisualBasic;
 namespace LeetCodeSolutions.BeeCrowd
 {
+
+    /// <summary>
+    /// 
+    /// Find the longest common substring between the two informed Strings. The substring can be any
+    /// part of the String, including the entire String. If there is no common substring, return 0. The search is case sensitive ('x' != 'X').
+
+    ///Input
+    //The input contains several test cases.Each test case is composed by two lines that contains a 
+    //  string each.Both input Strings will contain between 1 and 50, inclusive, letters (a-z, A-Z), and/or spaces.
+    /// </summary>
+    /// 
     public class FindLongestCommonSubtringTask
     {
         public int Solve(string str1, string str2)
         {
             int tot = 0;
-
-
-
             return 0;
         }
 
