b4800ddc74af5f7200adb6df9675c08d3f169dff
FrotaLucas <lucasdiasfrota@gmail.com>
2025-12-24 10:35:37 +0100

    cover new cases

--- LeetCodeSolutions/Codibility/EquiLeader.cs
+++ LeetCodeSolutions/Codibility/EquiLeader.cs
@@ -14,11 +14,10 @@ namespace LeetCodeSolutions.Codibility
 
 //    The leader of this array is the value that occurs in more than half of the elements of A.
 
-
-//    An equi leader is an index S such that 0 ≤ S<N − 1 and two sequences A[0], A[1], ..., A[S] and A[S + 1], A[S + 2], ..., A[N − 1] have leaders of the same value.
+//    An equi leader is an index S such that 0 ≤ S<N − 1 and two sequences A[0], A[1], ..., A[S] and A[S + 1], A[S + 2], ..., A[N − 1]
+//    have leaders of the same value.
 
 //    For example, given array A such that:
-
 
 //        A[0] = 4
     
@@ -75,11 +74,13 @@ namespace LeetCodeSolutions.Codibility
             int index = 0;
             int n = A.Length;
 
+            //esta erradooooooooooooooooooooooo isso aqui
             if (n <= 3)
                 return -1;
 
 
             int leader1 = A[0];
+
             int leader2 = A[1];
 
             while ( index < n-2 )
@@ -121,7 +122,13 @@ namespace LeetCodeSolutions.Codibility
 
             if (slice.Length == 1)
                 return slice[0];
-                
+
+            if (slice.Length == 2 && slice[0] == slice[1])
+                return slice[0];
+
+            if(slice.Length == 2 && slice[0] != slice[1])
+                return -1;
+
             int leader = slice[0];
 
             for (int j = 0; j < length; j++)
