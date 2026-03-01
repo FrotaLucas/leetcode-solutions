d3be71b9421cd083151aed790a91502fcb5dd8ed
FrotaLucas <lucasdiasfrota@gmail.com>
2026-03-01 23:18:39 +0100

    test of method

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -1,7 +1,9 @@
-﻿using LeetCodeSolutions.BeeCrowd;
+﻿using LeetCodeSolutions.AdventOfCode.Advent_2018;
+using LeetCodeSolutions.BeeCrowd;
 using LeetCodeSolutions.BeeCrowd.TdaRationalV1;
 using LeetCodeSolutions.BeeCrowd.TdaRationalV2;
 using LeetCodeSolutions.CoderByte;
+using LeetCodeSolutions.Codibility;
 
 
 
@@ -17,4 +19,23 @@ var tda2 = new TdaRationalTaskV2();
 var obj = new CountOccurancesAndFrequencyTask();
 obj.Solve("1;3;2;3;4;1;5;3");
 
-//3 1 
+
+var res = new EquiLeader();
+int[] A = [4, 3, 4, 4, 4, 2]; //true tem 2 leaders 
+int[] B = [3, 3, 3]; ///true tem 2 leaders
+int[] C = [3, 1, 1]; //false nao tem leader
+
+//ERA PARA PELO MENOS MOSTRAR 1 LEADER. 1 EH LIEDER
+int[] D = [3, 1, 1,       1, 2, 2, 1, 1]; //true  tem 3 leader
+
+
+//Console.WriteLine(res.Solve(D));
+
+var freq = new CountOfFrequency();
+
+string ids_string = "abcdef,bababc,abbcde,abcccd,aabcdd,abcdee,ababab";
+
+int r = freq.Solve(ids_string);
+
+Console.WriteLine(r);
+
