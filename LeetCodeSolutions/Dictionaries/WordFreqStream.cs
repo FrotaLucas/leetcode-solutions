1d077b432d0c5048dda83ceec68b993938bebd1c
FrotaLucas <lucasdiasfrota@gmail.com>
2026-03-25 12:28:13 +0100

    create new class

---
+++ LeetCodeSolutions/Dictionaries/WordFreqStream.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Dictionaries
+{
+    public class WordFreqStream
+    {
+
+        //create a method that should find the list of itemns more frequent
+        
+        //ex:
+
+        //string[] stream = ['apple', 'banana', 'apple', 'orange', 'apple', 'banana'];
+
+
+        //should return apple, banana
+        public string[] FindTopKeys(string[] stream)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            foreach (string key in stream)
+            {
+                if (dictionary.ContainsKey(key))
+                    dictionary[key]++;
+                else
+                    dictionary[key] = 1;
+            }
+
+            string[] keys = dictionary.Where( peer => peer.Value > 1).Select(peer => peer.Key).ToArray();
+
+            return keys;
+        }
+    }
+}
