0b79e82c74d28e113d694e3f7cb71511e9cec500
FrotaLucas <lucasdiasfrota@gmail.com>
2026-03-26 10:45:27 +0100

    rename folder

---
+++ LeetCodeSolutions/PreFixedSumAndDictionary/CountFreqDictionary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.PreFixedSumAndDictionary
+{
+    public class CountFreqDictionary
+    {
+        public Dictionary<int, int> Solve(int[] array)
+        {
+            Dictionary<int, int> dictionary = new Dictionary<int, int>(); 
+
+            foreach (int i in array)
+            {
+                if (!dictionary.ContainsKey(i))
+                    dictionary[i] = 1;
+
+                dictionary[i]++;    
+            }
+
+            Console.WriteLine(dictionary);
+
+            return dictionary;
+        }
+    }
+}
--- LeetCodeSolutions/PreFixedSumAndDictionary/SumFixSubArray.cs
+++ LeetCodeSolutions/PreFixedSumAndDictionary/SumFixSubArray.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LeetCodeSolutions.Dictionaries
+namespace LeetCodeSolutions.PreFixedSumAndDictionary
 {
 
     //    Exercícios Fáceis(3)
