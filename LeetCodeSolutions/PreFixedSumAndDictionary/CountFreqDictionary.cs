7e0224e67cf893606de7f1be4facbf47c9ff0829
FrotaLucas <lucasdiasfrota@gmail.com>
2026-03-26 14:47:44 +0100

    fix code

--- LeetCodeSolutions/PreFixedSumAndDictionary/CountFreqDictionary.cs
+++ LeetCodeSolutions/PreFixedSumAndDictionary/CountFreqDictionary.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LeetCodeSolutions.PreFixedSumAndDictionary
 {
+    //    3. Contar frequência de números
+
+    //Dado um array nums, retorne um dicionário com a contagem de cada número.
+
+    //Exemplo:
+
+    //nums = [1, 2, 2, 3, 1, 1]
+    //Resposta: { 1:3, 2:2, 3:1}
+
+    //💡 Dica: Use Dictionary direto.
+
     public class CountFreqDictionary
     {
         public Dictionary<int, int> Solve(int[] array)
@@ -17,10 +31,14 @@ namespace LeetCodeSolutions.PreFixedSumAndDictionary
                 if (!dictionary.ContainsKey(i))
                     dictionary[i] = 1;
 
-                dictionary[i]++;    
+                else 
+                    dictionary[i]++;
             }
 
-            Console.WriteLine(dictionary);
+            //print result
+            var result = string.Join(',', dictionary.Select(peer => $"{peer.Key}:{peer.Value}") );
+
+            Console.WriteLine(result);
 
             return dictionary;
         }
